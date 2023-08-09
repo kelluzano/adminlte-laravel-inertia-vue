92063dfb4e5ee262f5ec03d7d1ce1b16469207cd
kel <michaelluzano1997@gmail.com>
2023-08-09 16:16:16 +0800

    add restriction on inertia middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,7 +39,7 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'auth' => [
-                'user' => $request->user()->only(['name']),
+                'user' => $request->user()?->only(['name']),
             ],
             'ziggy' => function () {
                 return (new Ziggy)->toArray();
